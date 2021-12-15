9c8160d21fb64e0a1fac01a2887f00c04270b986
Maris GASPA <gmaris@student.42.fr>
2021-12-15 18:21:23 +0100

    first add stack is done

---
+++ Makefile
@@ -0,0 +1,71 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gmaris <gmaris@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/11/24 15:16:37 by gmaris            #+#    #+#              #
+#    Updated: 2021/12/15 18:20:39 by gmaris           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = tester
+
+CC = clang++
+FLAGS = -Wall -Wextra -Werror -std=c++98 -g #-fsanitize=address
+
+SRCS_DIR = ./
+
+SRC 		=	test.cpp
+
+INC			=	Stack.hpp\
+				Map.hpp\
+				Vector.hpp
+
+OBJS_DIR 	= 	./obj/
+OBJ 		= 	$(SRC:.cpp=.o)
+OBJS 		= 	$(addprefix $(OBJS_DIR), $(OBJ))
+
+TEST 		= 	stack_test.hpp
+
+TEST_DIR	= 	./test/
+TESTS 		= 	$(addprefix $(TEST_DIR), $(TEST))
+
+INC_DIR		= 	./includes/
+INCS		= 	$(addprefix $(INC_DIR), $(INC))
+INC_FLAG 	= 	-I$(INC_DIR) -I$(TEST_DIR)
+
+SRCS 		= 	$(addprefix $(SRCS_DIR), $(SRC))
+
+
+YELLOW = \033[033m
+GREEN = \033[032m
+BLUE = \033[36m
+RED = \033[031m
+PURPLE = \033[35m
+RESET = \033[0m
+
+$(OBJS_DIR)%.o :	$(SRCS_DIR)%.cpp $(INCS) $(TESTS)
+	@mkdir -p $(OBJS_DIR)
+	@echo "$(YELLOW)$(notdir $(basename $@))...$(RESET)"
+	@$(CC) $(INC_FLAG) $(FLAGS) -c $< -o $@
+	@echo "\t\t[$(GREEN)OK$(RESET)]"
+
+$(NAME): $(OBJS) Makefile $(INCS) $(TESTS)
+	@$(CC) $(INC_FLAG) $(FLAGS) -o $(NAME) $(OBJS)
+	@echo "[$(GREEN)$(NAME) compiled$(RESET)]"
+
+all: $(NAME)
+
+clean:
+	@echo "Cleaning: $(OBJS_DIR)"
+	@rm -rf $(OBJS_DIR)
+
+fclean: clean
+	@echo "Cleaning: $(NAME)"
+	@rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: fclean clean re all
---
+++ includes/Stack.hpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmaris <gmaris@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 14:59:09 by gmaris            #+#    #+#             */
+/*   Updated: 2021/12/15 18:19:12 by gmaris           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STACK_HPP
+#define FT_STACK_HPP
+
+#include <iostream>
+#include <vector>
+
+namespace ft{
+template< class T, class Container = std::vector<T> >
+class stack
+{
+	public:
+		/***************\
+		** member type **
+		\****************/
+		typedef T value_type;
+		typedef Container container_type;
+		typedef size_t size_type;
+
+	protected:
+		container_type c;
+	public:
+		/********************\
+		** member functions **
+		\********************/
+
+		//Constructor
+		explicit stack(const Container &cont = Container()): c(cont){}
+		~stack(){};
+		//size
+		size_type			size() const
+		{
+			return this->c.size();
+		}
+
+		//empty
+		bool				empty() const 
+		{
+			return this->c.empty();
+		}
+
+		//top
+		value_type&			top()
+		{
+			return this->c.back();
+		}
+		const value_type&	top() const
+		{
+			return this->c.back();
+		}
+
+		//push
+		void				push(const value_type& val)
+		{
+			this->c.push_back(val);
+		}
+
+		//pop
+		void				pop()
+		{
+			this->c.pop_back();
+		}
+		/************************\
+		** non-member functions **
+		\************************/
+		
+		//relational operators
+		template <class value_type, class container_type>
+		friend bool operator== (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c == rhs.c);
+		}
+		
+		template <class value_type, class container_type>
+		friend bool operator!= (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c != rhs.c);
+		}
+
+		template <class value_type, class container_type>
+		friend bool operator<  (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c <  rhs.c);
+		}
+
+		template <class value_type, class container_type>
+		friend bool operator<= (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c <= rhs.c);
+		}
+
+		template <class value_type, class container_type>
+		friend bool operator> (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c >  rhs.c);
+		}
+
+		template <class value_type, class container_type>
+		friend bool operator>= (const stack<value_type, container_type>& lhs, const stack<value_type, container_type>& rhs)
+		{
+			return (lhs.c >= rhs.c);
+		}
+};
+
+}
+#endif
