ecb3864de91ef9256bf29dab694defcce7f6d9b9
Maris GASPA <gmaris@student.42.fr>
2021-12-16 18:59:42 +0100

    progress

---
+++ includes/Iterator.hpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmaris <gmaris@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 15:02:07 by gmaris            #+#    #+#             */
+/*   Updated: 2021/12/16 15:27:42 by gmaris           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
+
+namespace ft{
+
+template<class T, class Alloc>
+class iterator
+{	
+	public:
+		/***************\
+		** member type **
+		\****************/
+		typedef T 								value_type;
+		typedef Alloc							allocator_type;
+		typedef allocator_type::reference		reference;
+		typedef allocator_type::pointer			pointer;
+		typedef allocator_type::const_pointer	const_pointer;
+		typedef ptrdiff_t 						difference_type;
+		typedef size_t 							size_type;
+	
+	public:
+		iterator(const iterator&);
+		~iterator();
+
+		iterator	&operator=(const iterator &lhs);
+		iterator	&operator++(); //prefix increment
+		reference	operator*();
+	
+	public:
+		int *p;
+};
+
+template<class T, class Alloc>
+class input_iterator : public virtual iterator<T, Alloc>
+{
+	public:
+		iterator	operator++(int); //postfix increment
+		value_type	operator*() const;
+		pointer		operator->();
+
+		friend bool	operator==(const iterator &rhs, const iterator &lhs);
+		friend bool	operator!=(const iterator &rhs, const iterator &lhs);
+};
+
+template<class T, class Alloc>
+class output_iterator : public virtual iterator<T, Alloc>
+{
+	public:
+		reference	operator*() const;
+		iterator	operator++(int); //postfix increment
+};
+
+
+template<class T, class Alloc>
+class forward_iterator : input_iterator<T, Alloc>, output_iterator<T, Alloc>
+{
+};
+
+template<class T, class Alloc>
+class bidirectional_iterator : forward_iterator<T, Alloc>
+{
+	iterator	&operator--(); //prefix decrement
+	iterator	&operator--(int); //postfix decrement
+};
+
+
+template<class T, class Alloc>
+class random_access_iterator : bidirectional_iterator<T, Alloc>
+{
+	iterator		&operator+=(size_type lhs);
+	iterator		&operator-=(size_type lhs);
+
+	friend bool		operator<(const iterator &rhs, const iterator &lhs);
+	friend bool		operator>(const iterator &rhs, const iterator &lhs);
+	friend bool		operator<=(const iterator &rhs, const iterator &lhs);
+	friend bool		operator>=(const iterator &rhs, const iterator &lhs);
+
+	friend iterator	operator+(const iterator &rhs, size_type lhs);
+	friend iterator	operator+(size_type rhs, const iterator &lhs);
+
+	friend iterator	operator-(const iterator &rhs, size_type lhs);
+	friend iterator	operator-(size_type rhs, const iterator &lhs);
+
+	reference operator[](size_type) const;
+
+}
+
+
+}
+#endif
--- includes/Vector.hpp
+++ includes/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: gmaris <gmaris@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:58:13 by gmaris            #+#    #+#             */
-/*   Updated: 2021/12/15 19:12:51 by gmaris           ###   ########.fr       */
+/*   Updated: 2021/12/16 15:08:44 by gmaris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,28 @@ namespace ft {
 template< class T, class Alloc = std::allocator<T> >
 class vector
 {
+	/******************\
+	** iterator class **
+	\******************/
+	template<
+
+
+
 	public:
 		/***************\
 		** member type **
 		\****************/
-		typedef T value_type;
-		typedef Alloc allocator_type;
-		typedef allocator_type::reference reference;
-		typedef allocator_type::pointer pointer;
-		typedef allocator_type::const_pointer const_pointer;
+		typedef T 								value_type;
+		typedef Alloc							allocator_type;
+		typedef allocator_type::reference		reference;
+		typedef allocator_type::pointer			pointer;
+		typedef allocator_type::const_pointer	const_pointer;
+		typedef ptrdiff_t 						difference_type;
+		typedef size_t 							size_type;
 		//typedef iterator iterator;
 		//typedef          const_iterator
 		//typedef          reverse_iterator
 		//typedef          const_reverse_iterator;
-		typedef ptrdiff_t difference_type;
-		typedef size_t size_type;
 
 	protected:
 
