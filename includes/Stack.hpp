d347891ecc223f02c4f5c7e28c3a8e1bdd2ef260
Maris GASPA <gmaris@student.42.fr>
2022-01-31 18:07:21 +0100

    better check removed an important part :)

--- includes/Stack.hpp
+++ includes/Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: gmaris <gmaris@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:59:09 by gmaris            #+#    #+#             */
-/*   Updated: 2022/01/18 14:42:42 by gmaris           ###   ########.fr       */
+/*   Updated: 2022/01/31 17:53:15 by gmaris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,13 @@ class stack
 		typedef Container container_type;
 		typedef size_t size_type;
 
-	protected:
-		container_type c;
 	public:
 		/********************\
 		** member functions **
 		\********************/
 
 		//Constructor
-		explicit stack(const Container &cont = Container()): c(cont){}
+		explicit stack(const container_type &cont = Container()): c(cont){}
 		~stack(){};
 		//size
 		size_type			size() const
@@ -110,6 +108,8 @@ class stack
 		{
 			return (lhs.c >= rhs.c);
 		}
+	protected:
+		container_type c;
 };
 
 }
